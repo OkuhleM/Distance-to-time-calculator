ef3a6a1650e8d2f5749e248f672847059ec4ce10
tcg-pc1 <lindokuhlea38@gmail.com>
2022-01-12 12:19:14 +0200

    saved source

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import DistanceToTime from './Components/DistanceToTime';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <DistanceToTime />
+    </div>
+  );
+}
+
+export default App;
